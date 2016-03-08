91d97f120d1d8d11f76a9b93415dc1c4bc5bd9bf
Paul Maddox <pmaddox@amazon.com>
2016-03-08 11:39:49 +0000

    Introduced failure

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-const VERSION string = "1.1.0"
+const VERSION string = "1.1.
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Current version: "+VERSION)
