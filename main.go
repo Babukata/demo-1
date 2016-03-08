af7e953d5fc424d2d4f4729b6d2da08bf0fe69c7
Paul Maddox <pmaddox@amazon.com>
2016-03-08 11:33:53 +0000

    Updated version

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ import (
 	"net/http"
 )
 
-const VERSION string = "1.0.9"
+const VERSION string = "1.1.0"
 
-func hello(w http.ResponseWriter, r *http.Request) {
+func rootHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Current version: "+VERSION)
 }
 
 func main() {
 	log.Printf("Listening on port 8000...")
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", rootHandler)
 	http.ListenAndServe(":8000", nil)
 }
