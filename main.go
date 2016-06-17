13beb5c3c8c38dd511c6b69290634b423f72e604
Paul Maddox <paul@sonelli.com>
2016-06-17 16:02:58 +0100

    Update main.go
    Bumped version

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 )
 
-const VERSION string = "1.1.1"
+const VERSION string = "1.1.2"
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Current version: "+VERSION)
