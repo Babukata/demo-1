3c8274c6bb9ef0e8d4c0f21b98d1afb497f6ae11
Paul Maddox <pmaddox@amazon.com>
2019-06-26 12:03:41 +0400

    Updated to simple go app that listens and also optionally creates requests if OUTBOUND env is set

--- main.go
+++ main.go
@@ -1,19 +1,59 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
-const VERSION string = "1.1.2"
+func main() {
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Current version: "+VERSION)
+	// If OUTBOUND is set, start a thread that creates constant outbound HTTP connections to that address
+	if outbound := os.Getenv("OUTBOUND"); outbound != "" {
+		go connect(outbound)
+	}
+
+	// Start a HTTP listener on port 80 that just logs all incoming requests, and their HTTP headers
+	createListener()
+
 }
 
-func main() {
-	log.Printf("Listening on port 8000...")
-	http.HandleFunc("/", rootHandler)
-	http.ListenAndServe(":8000", nil)
+// connect creates an outbound HTTP GET request every 100ms to the provided address
+func connect(outbound string) {
+	for {
+		time.Sleep(100 * time.Millisecond)
+		resp, err := http.Get(outbound)
+		if err != nil {
+			log.Printf("Outbound failure: %s", err)
+			continue
+		}
+		log.Printf("Outbound connection to %s responded with HTTP %d", outbound, resp.StatusCode)
+	}
+}
+
+func createListener() {
+
+	// Create a new HTTP router
+	r := mux.NewRouter()
+
+	// Respond on "/" with a simple echo of the incoming request details
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		for k, v := range r.Header {
+			fmt.Printf("    %s: %s\n", k, strings.Join(v, " "))
+		}
+	})
+
+	// Wrap the router in a logging middleware
+	logged := handlers.LoggingHandler(os.Stdout, r)
+
+	// Start the HTTP listener
+	log.Printf("Listening on port 80...")
+	log.Fatal(http.ListenAndServe(":80", logged))
+
 }
